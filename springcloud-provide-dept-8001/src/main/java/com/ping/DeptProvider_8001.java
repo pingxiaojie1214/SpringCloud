839110eff7a7a52ddfc43f215629a98d8aef1953
pxj <1194976108@qq.com>
2020-01-11 02:46:43 +0800

    Eureka学习 1.Euraka注册中心 2.提供者注册到注册中心 3.提供者在注册中心的信息，DiscoveryClient/Actuactor(作用不大)

--- springcloud-provide-dept-8001/src/main/java/com/ping/DeptProvider_8001.java
+++ springcloud-provide-dept-8001/src/main/java/com/ping/DeptProvider_8001.java
@@ -2,7 +2,12 @@ package com.ping;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 //启动类
+@EnableDiscoveryClient
+@EnableEurekaClient //服务启动会自动注入到Eureka注册中心。
 @SpringBootApplication
 public class DeptProvider_8001 {
     public static void main(String[] args) {
--- springcloud-provide-dept-8001/src/main/java/com/ping/controller/DeptController.java
+++ springcloud-provide-dept-8001/src/main/java/com/ping/controller/DeptController.java
@@ -3,6 +3,8 @@ package com.ping.controller;
 import com.ping.pojo.Dept;
 import com.ping.service.DeptService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -13,6 +15,9 @@ public class DeptController {
 
     @Autowired
     private DeptService deptService;
+
+    @Autowired
+    private DiscoveryClient client;
 
     @PostMapping("/dept/add")
     public boolean addDept(@RequestBody Dept dept){
@@ -28,6 +33,26 @@ public class DeptController {
     @GetMapping("/dept/list")
     public List<Dept> addDept(){
         return deptService.queryAll();
+    }
+
+//    获取一些注册进来的微服务的信息
+    @GetMapping("/dept/discovery")
+    public Object discovery(){
+        //获取微服务的列表清单
+        List<String> services = client.getServices();
+        System.out.println("discovery=>services:"+services);
+
+        //得到一个具体的微服务信息,通过id,applicationName;
+        List<ServiceInstance> instances = client.getInstances("springcloud-provider-dept");
+        for (ServiceInstance instance:instances) {
+            System.out.println(
+                    instance.getHost()+"\t"+
+                    instance.getPort()+"\t"+
+                    instance.getUri()+"\t"+
+                    instance.getServiceId()+"\t"
+            );
+        }
+        return this.client;
     }
 
 
